2feffee11025bbd3e704438529a64206d762ce3f
ackermanmoriii <ackermanmoriii@gmail.com>
2024-10-13 08:56:04 +0330

    rank: and title: , then changed their position at the function calling

--- test.rb
+++ test.rb
@@ -1,11 +1,13 @@
 
 
-def movie_info (title, rank = 5)
+def movie_info (title: , rank: )
 
     star = "⭐" * rank
 
     "#{title} has a rank of #{rank}: #{star}"
 end
+
+puts movie_info(rank: 9, title: "Breaking Bad")
 
 
 
